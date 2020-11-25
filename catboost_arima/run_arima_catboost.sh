ca46d4fe22d55abfaa8f5b96e25af50d72f75d9b
Kasun Bandara <kbandara149@gmail.com>
2020-11-25 20:23:57 +1100

    Update run_arima_catboost.sh

--- catboost_arima/run_arima_catboost.sh
+++ catboost_arima/run_arima_catboost.sh
@@ -12,5 +12,5 @@ python ./catboost_arima/CatBoostModel.py
 # run ARIMA_Catboost.R
 Rscript ./catboost_arima/ARIMA_Catboost.R
 
-# run ARIMA_Catboost.R
+# run Catboost_ARIMA_Aggregation.R
 Rscript ./catboost_arima/Catboost_ARIMA_Aggregation.R
