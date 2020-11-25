490fc650a17c95cb9db17e12deec2b6a137a8607
Kasun Bandara <kbandara149@gmail.com>
2020-11-25 19:46:59 +1100

    Update install_packages.py

--- catboost_arima/install_packages.py
+++ catboost_arima/install_packages.py
@@ -1,5 +1,9 @@
-# Install required packages
-pip install pandas
-pip install numpy
-pip install sklearn
-pip install catboost
+#!/usr/bin/env python
+
+from pip._internal import main as pip
+pip(['install', '--upgrade', 'pip'])
+pip(['install', '--user', 'pandas'])
+pip(['install', '--user', 'numpy'])
+pip(['install', '--user', 'sklearn'])
+pip(['install', '--user', 'catboost'])
+pip(['install', '--user', 'pyarrow'])
